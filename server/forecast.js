cc276907d3f074570ab8cec1719246fbffccb85a
Xen1k <xen1kdevelop@gmail.com>
2022-07-31 13:34:16 +0300

    Error Handling

--- server/forecast.js
+++ server/forecast.js
@@ -3,14 +3,13 @@ const router = Router();
 const axios = require('axios').default;
 
 router.post('/forecast', async (req, res) => {
-
-
-    const dataFromApi = await axios.get(`http://api.openweathermap.org/data/2.5/find?q=${req.body.city}&type=like&units=metric&lang=ru&APPID=ce1c597881d261bca53cbdbfe3403662`);
-
-    if (dataFromApi.data.list[0])
-        res.json(dataFromApi.data.list[0])
-    else
+    try {
+        const dataFromApi = await axios.get(`http://api.openweathermap.org/data/2.5/find?q=${req.body.city}&type=like&units=metric&lang=ru&APPID=ce1c597881d261bca53cbdbfe3403662`);
+        res.json(dataFromApi.data.list[0]);
+    }
+    catch {
         res.end(0);
+    }
 })
 
 
